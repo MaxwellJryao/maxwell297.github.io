01a0df4113e333770e08183e7b7cc3c674394ca2
maxwell297 <1565570382@qq.com>
2022-10-29 01:31:52 +0800

    Site updated: 2022-10-29 01:31:51

--- js/matery.js
+++ js/matery.js
@@ -174,7 +174,7 @@ $(function () {
 //黑夜模式提醒开启功能
 setTimeout(function () {
     if ((new Date().getHours() >= 19 || new Date().getHours() < 7) && !$('body').hasClass('DarkMode')) {
-        let toastHTML = '<span style="color:#97b8b2;border-radius: 10px;>' + '<i class="fa fa-bellaria-hidden="true"></i>晚上使用深色模式阅读更好哦。(ﾟ▽ﾟ)</span>'
+        let toastHTML = '<span style="color:#97b8b2;border-radius: 10px;>' + '<i class="fa fa-bellaria-hidden="true"></i>It\'s better to read in dark mode at night. ;)</span>'
         M.toast({ html: toastHTML })
     }
 }, 2200);
